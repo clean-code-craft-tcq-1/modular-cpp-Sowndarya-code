d088e459b196465363669333ceeb219f66cb8101
Sowndarya-code <77658898+Sowndarya-code@users.noreply.github.com>
2021-02-22 19:01:05 +0530

    Update c_color_pair.h

--- c_color_pair.h
+++ c_color_pair.h
@@ -1,4 +1,26 @@
 #ifndef C_COLOR_PAIR_H__
 #define C_COLOR_PAIR_H__
 
+
+ class ColorPair {
+       private:
+            MajorColor majorColor;
+            MinorColor minorColor;
+        public:
+            ColorPair(MajorColor major, MinorColor minor):
+                majorColor(major), minorColor(minor)
+            {}
+            MajorColor getMajor() {
+                return majorColor;
+            }
+            MinorColor getMinor() {
+                return minorColor;
+            }
+            std::string ToString() {
+                std::string colorPairStr = MajorColorNames[majorColor];
+                colorPairStr += " ";
+                colorPairStr += MinorColorNames[minorColor];
+                return colorPairStr;
+            }
+      
 #endif
