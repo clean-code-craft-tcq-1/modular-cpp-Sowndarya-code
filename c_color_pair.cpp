b62c49e3d2be0c0bd8feb23848024e044aa98074
Sowndarya-code <77658898+Sowndarya-code@users.noreply.github.com>
2021-02-22 19:11:20 +0530

    Update c_color_pair.cpp

--- c_color_pair.cpp
+++ c_color_pair.cpp
@@ -1,1 +1,20 @@
 #include "c_color_pair.h"
+
+namespace TelCoColorCoder
+{
+  MajorColor ColorPair::getMajor()
+  {
+    return majorColor;
+  }
+  MinorColor ColorPair::getMinor()
+  {
+    return minorColor;
+  }
+  std::string ColorPair::ToString()
+  {
+                    std::string colorPairStr = MajorColorNames[majorColor];
+                colorPairStr += " ";
+                colorPairStr += MinorColorNames[minorColor];
+                return colorPairStr;
+  }
+}
