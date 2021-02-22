a0493107f5343710e49b031ddf2970b0fad5ea6f
Sowndarya-code <77658898+Sowndarya-code@users.noreply.github.com>
2021-02-22 19:17:32 +0530

    Update c_color_pair.cpp

--- c_color_pair.cpp
+++ c_color_pair.cpp
@@ -6,15 +6,30 @@ namespace TelCoColorCoder
   {
     return majorColor;
   }
+  
   MinorColor ColorPair::getMinor()
   {
     return minorColor;
   }
+  
   std::string ColorPair::ToString()
   {
     std::string colorPairStr = MajorColorNames[majorColor];
     colorPairStr += " ";
     colorPairStr += MinorColorNames[minorColor];
     return colorPairStr;
+  }
+  
+  ColorPair ColorPair::GetColorFromPairNumber(int pairNumber)
+  {
+    int zeroBasedPairNumber = pairNumber - 1;
+    MajorColor majorColor = (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    MinorColor minorColor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return ColorPair(majorColor, minorColor);
+  }
+  
+  int GetPairNumberFromColor(MajorColor major, MinorColor minor)
+  {
+    return major * numberOfMinorColors + minor + 1;
   }
 }
