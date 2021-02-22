8f45ec35e46c66552204e0fdcc73368a9c3dbf76
Sowndarya-code <77658898+Sowndarya-code@users.noreply.github.com>
2021-02-22 20:29:57 +0530

    Create colorcode_encode_decode.cpp

---
+++ colorcode_encode_decode.cpp
@@ -0,0 +1,12 @@
+#include "colorcode_encode_decode.h"
+
+namespace TelCoColorCoder
+{
+    ColorPair GetColorFromPairNumber(int pairNumber) 
+    {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+    }
+}
