3e590dff20384d13d6dcab4cb52d305a51294e5d
Sowndarya-code <77658898+Sowndarya-code@users.noreply.github.com>
2021-02-22 20:26:51 +0530

    Update colorcode_encode_decode.h

--- colorcode_encode_decode.h
+++ colorcode_encode_decode.h
@@ -10,7 +10,10 @@ namespace TelCoColorCoder
  int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
  const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
  int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-  
+ 
+ ColorPair GetColorFromPairNumber(int pairNumber);
+ int GetPairNumberFromColor(MajorColor major, MinorColor minor);
+ 
 }// end of namespace TelCoColorCoder
 
 #endif
