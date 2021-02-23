395fa0113364426900b1fb051d38ff8e85a5f348
Sowndarya-code <77658898+Sowndarya-code@users.noreply.github.com>
2021-02-23 19:33:58 +0530

    Update colorcode_encode_decode.cpp

--- colorcode_encode_decode.cpp
+++ colorcode_encode_decode.cpp
@@ -6,6 +6,10 @@ namespace TelCoColorCoder
  //enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
  //int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
  //int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+  const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet" };
+ const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+      int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+ int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
     
     ColorPair GetColorFromPairNumber(int pairNumber) 
     {
