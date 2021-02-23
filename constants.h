69cfaadc3d4d67fc1bfcca22aeb0767789ed2ec3
Sowndarya-code <77658898+Sowndarya-code@users.noreply.github.com>
2021-02-23 20:18:33 +0530

    Update constants.h

--- constants.h
+++ constants.h
@@ -1,10 +1,10 @@
 #include <iostream>
 
-#ifndef CONSTANTS_H__
-#define CONSTANTS_H__
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
 namespace TelCoColorCoder
 {
-    //const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    //const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 }
 #endif
