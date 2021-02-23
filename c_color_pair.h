b1163cb2239092aa009f7ff49999a5d967de951c
Sowndarya-code <77658898+Sowndarya-code@users.noreply.github.com>
2021-02-23 19:13:50 +0530

    Update c_color_pair.h

--- c_color_pair.h
+++ c_color_pair.h
@@ -6,6 +6,15 @@
 
 namespace TelCoColorCoder
 {
+     enum class MajorColor : uint {WHITE, RED, BLACK, YELLOW, VIOLET};
+    enum class MinorColor : uint {BLUE, ORANGE, GREEN, BROWN, SLATE};
+ //extern enum MajorColor, MinorColor;
+ const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet" };
+ extern int numberOfMajorColors, numberOfMinorColors;
+ const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+ 
+ ColorPair GetColorFromPairNumber(int pairNumber);
+ int GetPairNumberFromColor(MajorColor major, MinorColor minor);
  class ColorPair 
  {
  public:
